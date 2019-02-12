f6ecdc42f4a00b765d5a313a690b5a04431ce9fb
Sean Tesch <tesch.sean@gmail.com>
2019-02-12 10:18:28 -0500

    Updated the sql file

--- JDBCBank.sql
+++ JDBCBank.sql
@@ -5,8 +5,22 @@ create table users (
     is_admin number(1) not null
 );
 
+drop table accounts;
+drop table users;
+
+create table accounts (
+    account_id number(10) not null primary key,
+    type_account varchar2(255) not null,
+    acc_user varchar(255) not null,
+    account_bal number(10,2),
+    constraint acc_username 
+        foreign key (acc_user) 
+        references users(username)
+);
+
+
 insert into users (user_id, username, pass, is_admin) values (1, 'root', 'p4ssw0rd', 1);
-insert into accounts (account_id, type_account, username, account_bal) values (1, 1, 'root', 0.00);
+insert into accounts (account_id, type_account, username, account_bal) values (1, 'admin', 'root', 0.00);
 
 CREATE SEQUENCE user_seq
     start with 1
@@ -39,90 +53,104 @@ BEGIN
 END;    
 /
 
-create or replace procedure addUser(username IN varchar2, pass IN varchar2, acc_type in number,
-                                    bal in number, is_admin number, user_id out number, account_id out number)
+create or replace procedure addUser(u_username IN varchar2, u_pass IN varchar2, isadmin in number, user_id out number)
 is
 begin
     insert into users (username, pass, is_admin) 
-    values(username, pass, is_admin);
+    values(u_username, u_pass, isadmin);
     user_id := user_seq.currval;
+    commit;
+end;
+/
 
-    insert into accounts (type_account, username, account_bal)
-    values(acc_type, username, bal);
+create or replace procedure addAccount(u_username in varchar2, bal in number, acc_type in varchar2, account_id out number)
+is
+begin
+    insert into accounts (type_account, acc_user, account_bal)
+    values(acc_type, u_username, bal);
     account_id := account_seq.currval;
     commit;
 end;
 /
 
-CREATE OR REPLACE PROCEDURE login(users_username    in varchar2,    upassword   in varchar2, 
-                                  passed            out number,     userid      out number,     isAdmin out number, 
-                                  accbal            out number,     accid       out number,     acctype out number) 
+create or replace procedure login(uusername    in varchar2,    upassword   in varchar2, 
+                                  passed       out number,     userid      out number,     isAdmin out number) 
 as
-    users_password varchar2(200);
-    userId number(10);
+    u_password varchar2(200);
+    user_Id number(10);
     user_is_admin number(10);
-    accId number(10);
-    acc_type number(2);
-    acc_bal number(10,2);
 begin
     select  pass, 
             user_id, 
-            is_admin,
-            account_id,
-            type_account,
-            account_bal 
-            
-    into    users_password, 
-            userId, 
-            user_is_admin, 
-            accId, 
-            acc_type, 
-            acc_bal
-            
-    from    users full join accounts
-    on   username = users_username;
-    
-    if users_password = upassword then
+            is_admin
+    into    u_password,
+            user_Id,
+            user_is_admin
+    from    users
+    where   username = uusername;
+
+    if  u_password = upassword then
         passed := 1;
-        userid := userId;
+        userid := user_Id;
         isAdmin := user_is_admin;
-        accbal := acc_bal;
-        accid := accId;
-        acctype := acc_type;
     else
         passed := 0;
         userid := 0;
         isAdmin := 0;
-        accbal := 0;
-        accid := 0;
-        acctype := 0;
     end if;
 end;
 /
 
-CREATE OR REPLACE PROCEDURE deleteUser(user_name IN varchar2, pass_word IN varchar2)
-IS
-BEGIN
-    delete  from users 
+create or replace procedure deleteUser(user_name in varchar2, pass_word in varchar2)
+is
+begin
+    delete from users 
     where username = user_name and pass = pass_word;
     commit;
-    delete from accounts
-    where username = user_name;
-    commit;
-END;
+end;
 /
 
-drop table accounts;
-drop table users;
+create or replace procedure deleteAccount(user_name in varchar2, pass_word in varchar2, acc_id in number)
+is
+begin
+    delete  from accounts 
+    where acc_user = user_name and account_id = acc_id;
+    commit;
+end;
+/
 
-create table accounts (
-    account_id number(10) not null primary key,
-    type_account number(2) not null,
-    username varchar(255) not null,
-    account_bal number(10,2),
-    constraint acc_username 
-        foreign key (username) 
-        references users(username)
-);
+create or replace procedure viewBalance(user_name in varchar2, acc_type out varchar2, acc_bal out number)
+as
+    acctype varchar2(255);
+    accbal number(10,2);
+begin
+    select type_account, account_bal
+    into acctype, accbal
+    from accounts
+    where acc_user = user_name;
+        
+    acc_type := acctype;
+    acc_bal := accbal;
+end;
+/
 
-commit;
+create or replace procedure changeBalance(user_name in varchar2, bal in number, acc_id in number)
+as
+begin
+    update accounts 
+    set account_bal = bal + (select account_bal 
+                               from accounts 
+                               where acc_user = user_name and account_id = acc_id)
+    where acc_user = user_name and account_id = acc_id;
+end;
+/
+
+create or replace procedure getAccount(user_name in varchar2, acc_id in number, bal out number)
+as
+begin
+    select  account_bal
+    into bal
+    from accounts
+    where acc_user = user_name and account_id = acc_id;
+end;
+/
